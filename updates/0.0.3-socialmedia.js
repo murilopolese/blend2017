884a00562db8cd7051a1c178619abd1b45d06c21
Murilo Polese <murilopolese@gmail.com>
2016-12-20 13:49:13 +0000

    Social media update

---
+++ updates/0.0.3-socialmedia.js
@@ -0,0 +1,8 @@
+exports.create = {
+	SocialMedia: [
+		{ order: 1, label: "Facebook", link: "https://www.facebook.com/blendfest.ca", site: "facebook" },
+		{ order: 2, label: "Twitter", link: "https://twitter.com/Blendfest", site: "twitter" },
+		{ order: 3, label: "Vimeo", link: "https://vimeo.com/wineaftercoffee", site: "vimeo" },
+		{ order: 4, label: "Instagram", link: "https://www.instagram.com/blend_fest/", site: "instagram" },
+	],
+};
