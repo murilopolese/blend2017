ecc80a4309f84c8effe00cc8e396d05ae3299c70
Murilo Polese <murilopolese@gmail.com>
2016-12-20 13:46:16 +0000

    Navigation update

---
+++ updates/0.0.2-navigation.js
@@ -0,0 +1,9 @@
+exports.create = {
+	Navigation: [
+		{ order: 1, label: "About", link: "/about", class: "about" },
+		{ order: 2, label: "Location", link: "/location", class: "location" },
+		{ order: 3, label: "Lineup", link: "/lineup", class: "lineup" },
+		{ order: 4, label: "Tickets", link: "/tickets", class: "tickets" },
+		{ order: 5, label: "Program", link: "/program", class: "program" }
+	],
+};
