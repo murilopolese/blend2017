6f23311b4cb762f0489ef3e5149fb8e6008c1653
Murilo Polese <murilopolese@gmail.com>
2016-12-30 01:00:59 +0000

    Character and Ticket models

---
+++ models/Character.js
@@ -0,0 +1,44 @@
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
+/**
+ * Character Model
+ * ==========
+ */
+var Character = new keystone.List('Character');
+
+Character.add({
+
+	ticketId: { type: Types.Text, required: true, initial: true, index: true },
+
+	name: { type: Types.Text, required: true, initial: true },
+	surname: { type: Types.Text, required: true, initial: true },
+	email: { type: Types.Text, required: true, initial: true },
+	message: { type: Types.Text, required: false, initial: true },
+	website: { type: Types.Text, required: false, initial: true },
+
+	blockAmount: { type: Types.Number, required: true, initial: true },
+	block1Shape: { type: Types.Text, required: true, initial: true },
+	block1Color: { type: Types.Text, required: true, initial: true },
+	block1Size: { type: Types.Text, required: true, initial: true },
+	block1Texture: { type: Types.Text, required: true, initial: true },
+	block1Hidden: { type: Types.Boolean, required: true, initial: true },
+	block2Shape: { type: Types.Text, required: true, initial: true },
+	block2Color: { type: Types.Text, required: true, initial: true },
+	block2Size: { type: Types.Text, required: true, initial: true },
+	block2Texture: { type: Types.Text, required: true, initial: true },
+	block2Hidden: { type: Types.Boolean, required: true, initial: true },
+	block3Shape: { type: Types.Text, required: true, initial: true },
+	block3Color: { type: Types.Text, required: true, initial: true },
+	block3Size: { type: Types.Text, required: true, initial: true },
+	block3Texture: { type: Types.Text, required: true, initial: true },
+	block3Hidden: { type: Types.Boolean, required: true, initial: true }
+
+	}
+);
+
+/**
+ * Registration
+ */
+Character.defaultColumns = 'name, surname, email';
+Character.register();
---
+++ models/Ticket.js
@@ -0,0 +1,24 @@
+var keystone = require( 'keystone' );
+var Types = keystone.Field.Types;
+
+/**
+ * Ticket Model
+ * ==========
+ */
+var Ticket = new keystone.List( 'Ticket' );
+
+Ticket.add({
+	ticketId: { type: Types.Text, initial: true, required: true, index: true },
+	name: { type: Types.Text, initial: true, required: true },
+	surname: { type: Types.Text, initial: true, required: true },
+	email: { type: Types.Email, initial: true, required: true },
+	company: { type: Types.Text, initial: true, required: true },
+	ticketType: { type: Types.Text, initial: true, required: true },
+	badgeName: { type: Types.Text, initial: true, required: true }
+});
+
+/**
+ * Registration
+ */
+Ticket.defaultColumns = 'ticketId, name, email';
+Ticket.register();
