0a2870eab85f19c4855cbf6a61e39f9007fb9f46
Murilo Polese <murilopolese@gmail.com>
2017-01-05 18:09:57 +0000

    Hidding hidden blocks on home page
    I can't believe how this bug went through.

--- public/js/main.js
+++ public/js/main.js
@@ -65,19 +65,40 @@ $(document).ready(function() {
 					if( selectedChars[ j ].website ) {
 						websiteLink ='<a class="website-link" target="_blank" href="' + selectedChars[ j ].website + '"></a>';
 					}
-					$(this).append(
-						'<div class="mini-character">' +
-						'<div class="mini-character-container">' +
-						'<div class="block block0 ' + selectedChars[ j ].blocks[ 0 ].shape + '-block ' + selectedChars[ j ].blocks[ 0 ].size + '-block ' + selectedChars[ j ].blocks[ 0 ].color + '-block ' + selectedChars[ j ].blocks[ 0 ].texture + '-block"></div>' +
-						'<div class="block block1 ' + selectedChars[ j ].blocks[ 1 ].shape + '-block ' + selectedChars[ j ].blocks[ 1 ].size + '-block ' + selectedChars[ j ].blocks[ 1 ].color + '-block ' + selectedChars[ j ].blocks[ 1 ].texture + '-block"></div>' +
-						'<div class="block block2 ' + selectedChars[ j ].blocks[ 2 ].shape + '-block ' + selectedChars[ j ].blocks[ 2 ].size + '-block ' + selectedChars[ j ].blocks[ 2 ].color + '-block ' + selectedChars[ j ].blocks[ 2 ].texture + '-block"></div>' +
-						websiteLink +
-						'<div class="mini-character-tooltip">' +
-						'<p><strong>' + selectedChars[ j ].name + ' ' + selectedChars[ j ].surname + '</strong></p>' +
-						'<p>' + selectedChars[ j ].message + '</p>' +
-						'</div>' +
-						'</div>' +
-						'</div>')
+
+					var html = '<div class="mini-character">' +
+					'<div class="mini-character-container">';
+
+					if (selectedChars[ j ].blocks[ 0 ].hidden == "false") {
+						html += '<div class="block block0 ' + selectedChars[ j ].blocks[ 0 ].shape + '-block '
+						+ selectedChars[ j ].blocks[ 0 ].size + '-block '
+						+ selectedChars[ j ].blocks[ 0 ].color + '-block '
+						+ selectedChars[ j ].blocks[ 0 ].texture + '-block"></div>';
+					}
+					if (selectedChars[ j ].blocks[ 1 ].hidden == "false") {
+						html += '<div class="block block1 ' + selectedChars[ j ].blocks[ 1 ].shape + '-block '
+						+ selectedChars[ j ].blocks[ 1 ].size + '-block '
+						+ selectedChars[ j ].blocks[ 1 ].color + '-block '
+						+ selectedChars[ j ].blocks[ 1 ].texture + '-block"></div>';
+					}
+					if (selectedChars[ j ].blocks[ 2 ].hidden == "false") {
+						html += '<div class="block block2 ' + selectedChars[ j ].blocks[ 2 ].shape + '-block '
+						+ selectedChars[ j ].blocks[ 2 ].size + '-block '
+						+ selectedChars[ j ].blocks[ 2 ].color + '-block '
+						+ selectedChars[ j ].blocks[ 2 ].texture + '-block"></div>'
+					}
+
+					html += websiteLink +
+
+					'<div class="mini-character-tooltip">' +
+					'<p><strong>' + selectedChars[ j ].name + ' ' + selectedChars[ j ].surname + '</strong></p>' +
+					'<p>' + selectedChars[ j ].message + '</p>' +
+					'</div>' +
+
+					'</div>' +
+					'</div>';
+
+					$(this).append( html );
 				} else {
 					$(this).append(
 						'<div class="mini-character"></div>'
